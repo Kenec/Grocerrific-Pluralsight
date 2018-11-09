94c851d987aff0fe37cbf063b77d1ff07a0fceec
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-09 23:00:30 +0100

    fix test

--- client/__tests__/components/App.test.js
+++ client/__tests__/components/App.test.js
@@ -1,10 +1,13 @@
 /* global expect */
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../../components/App';
+import { App } from '../../components/App';
 
 describe('<App />', () => {
-  const wrapper = shallow(<App />);
+  const props = {
+    groceries: []
+  }
+  const wrapper = shallow(<App {...props}/>);
   it('should render the App contents', () => {
     expect(wrapper.find('div').at(0).length).toEqual(1);
     expect(wrapper.find('Header').at(0).length).toEqual(1);
