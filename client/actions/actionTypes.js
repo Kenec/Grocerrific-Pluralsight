204e09d9e38236ecddc816a7a6909436c965a8c8
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-10 20:25:12 +0100

    add, update and delete grocery

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -1,4 +1,4 @@
 export const ADD_GROCERY = "ADD_GROCERY";
-export const EDIT_GROCERY = "EDIT_GROCERY";
 export const LOAD_GROCERIES = "LOAD_GROCERIES";
 export const REMOVE_GROCERY = "REMOVE_GROCERY";
+export const BUY_OR_DROP_GROCERY = "BUY_OR_DROP_GROCERY";
--- client/reducers/groceryReducer.js
+++ client/reducers/groceryReducer.js
@@ -4,11 +4,20 @@ import initialState from './initialState';
 export default function groceryReducer(state = initialState.groceries, action) {
   switch(action.type) {
     case types.LOAD_GROCERIES:
-      return state;
+      return action.groceries;
 
     case types.REMOVE_GROCERY:
       return [
         ...state.filter(grocery => grocery.id !== action.id)
+      ];
+
+    case types.ADD_GROCERY:
+      return [...state, Object.assign({}, action.grocery)];
+
+    case types.BUY_OR_DROP_GROCERY:
+      return [
+        Object.assign({}, action.grocery),
+        ...state.filter(grocery => grocery.id !== action.grocery.id)
       ];
       
     default:
